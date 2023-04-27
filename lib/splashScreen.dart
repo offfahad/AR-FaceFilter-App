a28e7724e9d158478be34882c781922401a00d78
offfahad <mughalfahad544@gmail.com>
2023-04-27 08:53:46 +0500

    Splash Screen Done

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-class SplashScreen extends StatefulWidget {
+class MySplashScreen extends StatefulWidget {
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<MySplashScreen> createState() => _MySplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _MySplashScreenState extends State<MySplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SplashScreen(
+      seconds: 10,
+      title: Text(
+        "AR  Face  Filters  App",
+        style: TextStyle(
+          fontSize: 55,
+          color: Colors.deepPurpleAccent,
+          fontFamily: "Signatra",
+        ),
+      ),
+      image: Image.asset("images/icon.png"),
+      backgroundColor: Colors.white,
+      photoSize: 140,
+      loaderColor: Colors.deepPurple,
+      loadingText: Text(
+        "from Fahad",
+        style: TextStyle(
+          color: Colors.deepPurpleAccent,
+          fontSize: 16.0,
+          fontFamily: "Brand Bold",
+        ),
+      ),
+    );
   }
 }
